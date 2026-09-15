f53779a1b845ebb64a0191f8059546a76bd35a85
amidyou <avivaviv11223344@gmail.com>
2026-09-15 16:47:13 +0300

    researched rumble for gamepad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/rumbletest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+
+@TeleOp(name = "RumbletestTelep", group = "main")
+public class rumbletest extends CommandOpMode {
+    // this is the setting up of gamepad rumble
+    Gamepad.RumbleEffect.Builder rumble = new Gamepad.RumbleEffect.Builder();
+
+    @Override
+    public void initialize() {
+        TeleopTemplate.apply(this);
+        // this is the setting up the rumble
+        rumble.addStep(0.5,0.5,2000);
+        //this is running the rumble
+        gamepad1.runRumbleEffect(rumble.build());
+    }
+
+
+
+
+}
+
+
+
